b86de2af043344bc97c8cee7c063977f055bad1c
Tharik J.H <startharik@gmail.com>
2024-01-04 00:14:30 +0800

    Update minimax.py

--- py_version/minimax.py
+++ py_version/minimax.py
@@ -9,8 +9,8 @@ from os import system
 An implementation of Minimax AI Algorithm in Tic Tac Toe,
 using Python.
 This software is available under GPL license.
-Author: Clederson Cruz
-Year: 2017
+Author: Gadora
+Year: 2023
 License: GNU GENERAL PUBLIC LICENSE (GPL)
 """
 
